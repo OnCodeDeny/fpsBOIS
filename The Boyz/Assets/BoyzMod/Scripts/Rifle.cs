a6adac776022bdeecbb735aa8adc0cc95e3044e0
OnCodeDeny <48635951+OnCodeDeny@users.noreply.github.com>
2020-01-26 19:18:54 -0500

    Add trigger mode.
    Add single shot and burst mode for rifle.

---
+++ The Boyz/Assets/BoyzMod/Scripts/Rifle.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rifle : MonoBehaviour
+{
+    public enum TriggerMode
+    {
+        Single,
+        //Duo,
+        Burst,
+    }
+
+    public TriggerMode currentTriggerMode;
+
+    WeaponController currentController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentController = GetComponent<WeaponController>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("TriggerModeSwitch"))
+        {
+            if (currentTriggerMode == TriggerMode.Burst)
+            {
+                currentTriggerMode = TriggerMode.Single;
+            }
+            else
+            {
+                currentTriggerMode++;
+            }
+        }
+        if (currentTriggerMode == TriggerMode.Single)
+        {
+            currentController.bulletsPerShot = 1;
+        }
+        else if (currentTriggerMode == TriggerMode.Burst)
+        {
+            currentController.bulletsPerShot = 3;
+        }
+    }
+}
